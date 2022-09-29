3817f72b30a8fb3bff50881d0df9feb6f2d0fbec
dvorak-hdez-mor <miguel.hernandezmor@alumno.buap.mx>
2022-09-28 22:36:13 -0500

    ADD: Nuevos patrones de colores en FS, gui para control

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import './style.css'
 
 import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+import {GUI} from 'dat.gui';
+import {MathUtils} from 'three';
 
 let scene = new THREE.Scene();
 let camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -20,10 +22,13 @@ let clock = new THREE.Clock();
 
 let controls = new OrbitControls(camera, renderer.domElement);
 
+// set gui
+const gui = new GUI();
+
 var axesHelper = new THREE.AxesHelper(40);
 scene.add(axesHelper);
 
-var boxGeometry = new THREE.BoxGeometry(80, 2, 80, 20, 1, 20);
+var boxGeometry = new THREE.BoxGeometry(160, 2, 160, 50, 1, 50);
 
 function getRandomVal(inf, sup){
 	let r = THREE.MathUtils.randFloat(inf, sup);
@@ -32,8 +37,7 @@ function getRandomVal(inf, sup){
 }
 
 let randomVal = getRandomVal(0.1, 3.9);
-
-alert(randomVal);
+//alert(randomVal);
 
 const uniformsData = {
 	u_time: {
@@ -136,8 +140,19 @@ const colorNormal_FS = `
 	uniform float u_time;
 
 	varying vec3 pos;
+
 	void main(){
-		gl_FragColor = vec4(0.0, 0.7, 0.4, 1.0);
+		gl_FragColor = vec4(tan(pos.x)-8.0*sin(pos.z/4.0 + u_time), 0.0, sin(1.5*u_time), 1.0);
+	}
+`;
+
+const colorNormal2_FS = `
+	uniform float u_time;
+
+	varying vec3 pos;
+
+	void main(){
+		gl_FragColor = vec4(abs(sin(u_time)), 0.0, tan(pos.z)-8.0*sin(pos.x/4.0 + u_time), 1.0);
 	}
 `;
 
@@ -159,26 +174,41 @@ var material = new THREE.ShaderMaterial({
 	wireframe: false,
 	uniforms: uniformsData,
 	vertexShader: mareaTranquila_VS,
-	fragmentShader: colorPsico_FS
+	fragmentShader: colorNormal_FS,
 });
 
-material.setValues({wireframe:true}); // apaga o enciende el wireframe
+var material2 = new THREE.ShaderMaterial({
+	wireframe: false,
+	uniforms: uniformsData,
+	vertexShader: mareaTranquila_VS,
+	fragmentShader: colorNormal2_FS,
+});
+
+material.setValues({wireframe:false}); // apaga o enciende el wireframe
 material.setValues({vertexShader:mareaTranquila_VS});
 
 // uniendo con el shader
 var box = new THREE.Mesh(boxGeometry, material);
 scene.add(box);
 
-var sphereGeometry = new THREE.SphereGeometry(5, 10, 10);
-var sphere = new THREE.Mesh(sphereGeometry, material);
-sphere.position.set(0, 5, 0);
+// Elementos del gui
+gui.add(box.material, 'wireframe');
+gui.add(uniformsData.u_random, "value", -3.9, 3.9, 0.1).name('Fuerza del Oleaje');
+//gui.add(opc, 'isPsico');
+
+var sphereGeometry = new THREE.SphereGeometry(6, 20, 20);
+var sphere = new THREE.Mesh(sphereGeometry, material2);
+sphere.position.set(0, 6, 0);
 scene.add(sphere);
 
-camera.position.set(0, 15, 25);
+camera.position.set(-15, 15, 15);
 
 camera.lookAt(0,0,0);
 
-const light = new THREE.AmbientLight(0xffffff);
+//const light = new THREE.AmbientLight(0xffffff);
+//scene.add(light);
+const light = new THREE.PointLight(0xffffff, 0.5, 10);
+light.position.set(0, 20, 0);
 scene.add(light);
 
 controls.update();
@@ -190,21 +220,13 @@ function onWindowResize( event ) {
 	renderer.setSize( window.innerWidth, window.innerHeight );
 }
 
-let step = 0;
-
-//alert(4.0 % 2==0);
-
-function animate(){
+function animate(now){
 	requestAnimationFrame(animate);
 	controls.update();
 
 	// actualiza uniforms
 	uniformsData.u_time.value = clock.getElapsedTime();
-	if (step % 3 == 0){
-		uniformsData.u_random.value = getRandomVal(0.1, 3.9);
-	}
-
-	step += 0.1;
+	//step += 0.1;
 
 	renderer.render(scene, camera);
 }
